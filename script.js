dac0d211c6c5a3893156f0e181b3908b2f174d25
Liza Odish <lizaodishl@gmail.com>
2023-02-14 08:46:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,7 @@ const weatherInfo = document.querySelector('#weather-info');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const city = input.value;
-    
+    const apiKey= '7ddee7b2da9d1fddabdcc7a324387e61';
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`';
+    fetch(url)
 }
