5ee716d0dbebcb38ae277bc40e5f1fe2b86fde2b
Liza Odish <lizaodishl@gmail.com>
2023-02-14 08:34:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const form = document.querySelector('form');
 const input = document.querySelector('#city');
 const weatherInfo = document.querySelector('#weather-info');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const city = input.ariaValueMax;
+}
